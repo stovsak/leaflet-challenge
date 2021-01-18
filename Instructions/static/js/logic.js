03490a62b42cfdd702d43ae4f12cd7f8e75e0723
STEVE OVSAK <steveovsak@MacBook-Pro.local>
2021-01-18 10:12:53 -0700

    mapping code

--- Instructions/static/js/logic.js
+++ Instructions/static/js/logic.js
@@ -13,3 +13,22 @@ function markerColor(mag) {
         return "#F02908";
     };
 }
+
+// Create the createMap function
+function createMap(earthquakes) {
+
+    // Create 2 tile layer options for the map
+    var satelitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.satellite",
+        accessToken: API_KEY
+    });
+
+    var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.dark",
+        accessToken: API_KEY
+    });
+}
