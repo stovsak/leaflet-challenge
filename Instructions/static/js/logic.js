5cacbf56ae042e36aa4092a5ea3ab7ecf3e8a0c5
STEVE OVSAK <steveovsak@MacBook-Pro.local>
2021-01-20 13:18:59 -0700

    mapping

--- Instructions/static/js/logic.js
+++ Instructions/static/js/logic.js
@@ -18,17 +18,17 @@ function markerColor(mag) {
 function createMap(earthquakes) {
 
     // Create 2 tile layer options for the map
-    var satelitemap = L.tileLayer("https://api.tiles.mapbox.com/styles/v4/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    var satelitemap = L.tileLayer("https://api.tiles.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         maxZoom: 18,
-        id: "mapbox.satellite",
+        id: "satellite-v9",
         accessToken: API_KEY
     });
 
-    var darkmap = L.tileLayer("https://api.tiles.mapbox.com/styles/v4/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    var darkmap = L.tileLayer("https://api.tiles.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         maxZoom: 18,
-        id: "mapbox.dark",
+        id: "dark-v10",
         accessToken: API_KEY
     });
 
